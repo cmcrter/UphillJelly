a8ecda8a3a54c0033c4732cf3ceebff7c0113b5f
Peedeedle <s1806443@glos.ac.uk>
2021-09-29 16:42:56 +0100

    Setup Replay basics

---
+++ Assets/Scripts/Replay System/GhostReplay.cs
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////
+// File:            GhostReplay.cs
+// Author:          Jack Peedle
+// Date Created:    29/09/21 
+// Brief:           Ghost replay script which controls the temporary movement of a test character as well as recording and replaying 
+//                  the characters movement and rotation to act as a "Ghost"
+//////////////////////////////////////////////////////////// 
+
+using UnityEngine;
+
+public class GhostReplay : MonoBehaviour 
+{
+    #region Public Fields
+
+    #endregion
+
+    #region Unity Methods
+
+void Start() {
+
+}
+
+void Update() {
+
+}
+    #endregion
+ 
+    #region Private Methods
+    #endregion
+}
