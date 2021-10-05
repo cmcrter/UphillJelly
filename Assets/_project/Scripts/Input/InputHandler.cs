edeaaf8bd666f16918c0ac6ca578b639edf5cbdc
Charles Carter <47723974+cmcrter@users.noreply.github.com>
2021-10-05 16:51:02 +0100

    Starting thinking more about input handling

---
+++ Assets/_project/Scripts/Input/InputHandler.cs
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////
+// File: InputHandler.cs
+// Author: Charles Carter
+// Date Created: 05/10/21
+// Last Edited By: Charles Carter
+// Date Last Edited: 05/10/21
+// Brief: A script which triggers any triggerable when the collider touches them (generally it'll be only players that has this on them)
+//////////////////////////////////////////////////////////// 
+
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    #region Public Fields
+    #endregion
+ 
+    #region Unity Methods
+    void Start()
+    {
+	
+    }
+ 
+    void Update()
+    {
+	
+    }
+    #endregion
+ 
+    #region Private Methods
+    #endregion
+}
