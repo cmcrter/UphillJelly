8367867c6640f172ee2449d73f42dcbb1861f602
Peedeedle <s1806443@glos.ac.uk>
2021-10-12 14:53:48 +0100

    Test1

---
+++ Assets/_project/Scripts/ProfileSystem/ProfileController.cs
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////
+// File: ProfileController.cs
+// Author: Jack Peedle
+// Date Created: 12/10/21
+// Last Edited By: Jack Peedle
+// Date Last Edited: 12/10/21
+// Brief: A script to control the profile system
+//////////////////////////////////////////////////////////// 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ProfileController : MonoBehaviour
+{
+
+    #region Variables
+
+    //
+    public Text nameInput;
+
+    #endregion
+
+    #region Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+}
