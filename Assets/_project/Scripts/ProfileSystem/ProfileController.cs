923a1eb570f4260dc45837a0693b756de9dfbb07
Peedeedle <s1806443@glos.ac.uk>
2021-10-18 11:42:57 +0100

    Save System revamped to player data

--- Assets/_project/Scripts/ProfileSystem/ProfileController.cs
+++ Assets/_project/Scripts/ProfileSystem/ProfileController.cs
@@ -12,12 +12,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.IO;
 
 
 public class ProfileController : MonoBehaviour
 {
 
     #region Variables
+
+    // Header to show the word data and a string for the players name
+    [Header("Data")]
+    public new string name = "Player"; 
+
+    // Reference to the TextMeshPro input field
+    public TMP_InputField TMPProfileTextInput;
 
     // array of gameobjects for the various hats
     public GameObject[] go_hats;
@@ -40,9 +49,17 @@ public class ProfileController : MonoBehaviour
     // Gameobject for the previous hat
     public GameObject go_previousHat;
 
+    //
+    public Material CurrentActivePlayerMaterial;
 
     //
     public GameObject currentActiveHat;
+
+    //
+    public GameObject hatSpawnPosition;
+
+    //
+    //public GameObject skateboardSpawnPosition;
 
     // int for the current outfit
     private int iCurrentOutfit;
@@ -58,6 +75,17 @@ public class ProfileController : MonoBehaviour
 
     #region Methods
 
+    // On Awake
+    void Awake() {
+
+        // save the hats to the list of save data
+        SaveSystem.Hats.Add(this);
+
+        // save the player materials to the list of save data
+        SaveSystem.PlayerMaterials.Add(this);
+
+    }
+
     // On Start
     public void Start() {
 
@@ -67,20 +95,17 @@ public class ProfileController : MonoBehaviour
         // current outfit = 0
         iCurrentOutfit = 0;
 
-        //
+        // On start set the current hat to 4 = no hat
         iCurrentHat = 4;
 
-        //
+        // set the gameobject array for the current hat to true
         go_hats[iCurrentHat].SetActive(true);
 
-        //
+        // set the previous hat to 4 in the hat array
         go_previousHat = go_hats[4];
 
         // set the players mesh renderer material to the character meshes current outfit
         playerMeshRenderer.material = mat_CharacterMeshes[iCurrentOutfit];
-
-        //
-        //go_previousHat = go_hats[iCurrentHat];
 
     }
 
@@ -90,12 +115,15 @@ public class ProfileController : MonoBehaviour
         // set the currency to the text to visualize the current currency to the player
         CurrencyText.text = "£" + Currency.ToString();
 
+        // Continuously set the previous hat to false
         go_previousHat.SetActive(false);
 
-
+        // set the current active hat to the current hat in the hats array
         currentActiveHat = go_hats[iCurrentHat];
 
-        
+        // set the current active hat to the current hat in the hats array
+        CurrentActivePlayerMaterial = playerMeshRenderer.material;
+
 
     }
 
@@ -177,38 +205,34 @@ public class ProfileController : MonoBehaviour
     // Change the hat to the right
     public void ChangeHatRight() {
 
-
-
-        // if the current outfit is less than or = to the length of meshes
-        // Show the first material / outfit
+        // if the current hat is more than 10
+        // Set the current hat to 1 which is the bottom of the array
         if (iCurrentHat > 10) {
 
-            // set the current outfit to 0
+            // set the hat to 0
             iCurrentHat = 1;
 
-
-            //
+            // set the previous hat to the 11th hat
             go_previousHat = go_hats[11];
 
         } else {
 
-            // current outfit + (move to the next one)
+            // increment the current hat +
             iCurrentHat++;
 
-            //
+            // set the previous hat to the current hat - 1
             go_previousHat = go_hats[iCurrentHat - 1];
 
         }
 
 
-        // for each character materials in the list
+        // for each hat in the list
         for (int i = 0; i < go_hats.Length; i++) {
 
-            // if current outfit is = 1, show mat 1 etc
+            // if current hat is = i
             if (iCurrentHat == i) {
 
-                // change the material to the current integer of the outfit
-                //go_HatSpawnPoint = go_hats[iCurrentHat];
+                // Set the current hat to active
                 go_hats[iCurrentHat].SetActive(true);
 
             }
@@ -226,41 +250,38 @@ public class ProfileController : MonoBehaviour
 
 
 
-    // Change the hat to the right
+    // Change the hat to the Left
     public void ChangeHatLeft() {
 
-        
-
-        // if the current outfit is less than or = to the length of meshes
-        // Show the first material / outfit
+        // if the current hat is less than 2
+        // Set the current hat to 11 which is the top of the array
         if (iCurrentHat < 2) {
 
-            // set the current outfit to 0
+            // set the hat to 11
             iCurrentHat = 11;
 
 
-            //
+            // set the previous hat to the 1st hat
             go_previousHat = go_hats[1];
 
         } else {
 
-            // current outfit + (move to the next one)
+            // increment the current hat -
             iCurrentHat--;
 
-            //
+            // set the previous hat to the current hat + 1
             go_previousHat = go_hats[iCurrentHat + 1];
 
         }
 
 
-        // for each character materials in the list
+        // for each hat in the list
         for (int i = 0; i < go_hats.Length; i++) {
 
-            // if current outfit is = 1, show mat 1 etc
+            // if current hat is = i
             if (iCurrentHat == i) {
 
-                // change the material to the current integer of the outfit
-                //go_HatSpawnPoint = go_hats[iCurrentHat];
+                // Set the current hat to active
                 go_hats[iCurrentHat].SetActive(true);
 
             }
