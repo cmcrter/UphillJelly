0e2996c7f6d8b55ca6e76c7d88cf02d72dcbb3eb
Matthew Mason <mattg.mason@outlook.com>
2022-01-28 14:23:19 +0000

    Worked on making the bird boid AI

--- Assets/_project/Scripts/Boids/BirdBoid.cs
+++ Assets/_project/Scripts/Boids/BirdBoid.cs
@@ -7,9 +7,12 @@ namespace L7Games.Boids
     public class BirdBoid : Boid
     {
 
+        private bool isIdle;
+
         public float seekWeight;
         public float fleeWeight;
         public float wanderWeight;
+        public float avoidWeight;
 
         public float wanderJitter;
         public float wanderForward;
@@ -19,52 +22,99 @@ namespace L7Games.Boids
 
         Vector3 currentVelocity;
 
-        public Transform fleeTransform;
+        public Transform playerTransform;
+
+        private void Start()
+        {
+            isIdle = true;
+        }
 
         // Update is called once per frame
         void Update()
         {
-            Vector3 newVelocity = Vector3.zero;
 
-            //if (fleeWeight > 0f)
-            //{
-            //    Vector3 fleeVelocity = GetFleeForce(maxSpeed, fleeTransform.position, transform.position, currentVelocity) * fleeWeight;
-            //    newVelocity += fleeVelocity;
-            //    if (newVelocity.magnitude > maxSpeed)
-            //    {
-            //        currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
-            //        transform.position += currentVelocity;
-            //        return;
-
-            //    }
-            //}
-            if (seekWeight > 0f)
+            if (!isIdle)
             {
-                Vector3 seekVelocity = GetSeekForce(maxSpeed, transform.position + Vector3.up, transform.position, currentVelocity) * seekWeight;
-                newVelocity += seekVelocity;
-                if (newVelocity.magnitude > maxSpeed)
-                {
-                    currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
-                    transform.position += currentVelocity;
-                    return;
-                }
-            }
+                Vector3 newVelocity = Vector3.zero;
 
-            if (wanderWeight > 0f)
-            {
-                Vector3 wanderVelocity = GetWanderForce(wanderRadius, wanderJitter, wanderForward, maxSpeed, currentVelocity, transform.position) * wanderWeight;
-                newVelocity += wanderVelocity;
-                if (newVelocity.magnitude > 10f)
-                {
-                    currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
-                    transform.position += currentVelocity;
-                    return;
-                }
-            }
+                //if (fleeWeight > 0f)
+                //{
+                //    Vector3 fleeVelocity = GetFleeForce(maxSpeed, fleeTransform.position, transform.position, currentVelocity) * fleeWeight;
+                //    newVelocity += fleeVelocity;
+                //    if (newVelocity.magnitude > maxSpeed)
+                //    {
+                //        currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
+                //        transform.position += currentVelocity;
+                //        return;
 
-            currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
-            transform.position += currentVelocity;
-            return;
+                //    }
+                //}
+                //if (seekWeight > 0f)
+                //{
+                //    Vector3 seekVelocity = GetSeekForce(maxSpeed, transform.position + Vector3.up, transform.position, currentVelocity) * seekWeight;
+                //    newVelocity += seekVelocity;
+                //    if (newVelocity.magnitude > maxSpeed)
+                //    {
+                //        currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed);
+                //        transform.position += currentVelocity;
+                //        return;
+                //    }
+                //}
+                    if (fleeWeight > 0f)
+                    {
+                        Vector3 fleeVelocity = GetFleeForce(maxSpeed * Time.deltaTime, playerTransform.position, transform.position, currentVelocity) * fleeWeight;
+                        newVelocity += fleeVelocity;
+                        if (newVelocity.magnitude > maxSpeed * Time.deltaTime)
+                        {
+                            currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed * Time.deltaTime);
+                            transform.position += currentVelocity;
+                            transform.forward = currentVelocity.normalized;
+                            return;
+
+                        }
+                    }
+
+                if (Vector3.Distance(playerTransform.position, transform.position) < 10f)
+                {
+                    if (fleeWeight > 0f)
+                    {
+                        Vector3 fleeVelocity = GetFleeForce(maxSpeed * Time.deltaTime, playerTransform.position, transform.position, currentVelocity) * fleeWeight;
+                        newVelocity += fleeVelocity;
+                        if (newVelocity.magnitude > maxSpeed * Time.deltaTime)
+                        {
+                            currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed * Time.deltaTime);
+                            transform.position += currentVelocity;
+                            transform.forward = currentVelocity.normalized;
+                            return;
+
+                        }
+                    }
+                }
+
+                if (wanderWeight > 0f)
+                {
+                    Vector3 wanderVelocity = GetWanderForce(wanderRadius, wanderJitter, wanderForward, maxSpeed, currentVelocity, transform.position) * wanderWeight;
+                    newVelocity += wanderVelocity;
+                    if (newVelocity.magnitude > maxSpeed * Time.deltaTime)
+                    {
+                        currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed * Time.deltaTime);
+                        transform.position += currentVelocity;
+                        transform.forward = currentVelocity.normalized;
+                        return;
+                    }
+                }
+
+                currentVelocity = Vector3.ClampMagnitude(newVelocity, maxSpeed * Time.deltaTime);
+                transform.position += currentVelocity;
+                transform.forward = currentVelocity.normalized;
+                return;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            playerTransform = other.transform;
+            isIdle = false;
         }
     }
 }
