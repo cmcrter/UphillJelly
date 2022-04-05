6b4dc561a2b61be947d774b0da5d3fd94954d2f8
Peedeedle <s1806443@glos.ac.uk>
2022-04-05 12:26:59 +0100

    Mall Glass Smash works (needs tweaking)

--- Assets/_project/S1806443 Work/Glass Shattter/DestructableWall.cs
+++ Assets/_project/S1806443 Work/Glass Shattter/DestructableWall.cs
@@ -6,24 +6,62 @@ namespace SleepyCat
 {
     public class DestructableWall : MonoBehaviour {
 
+        //public L7Games.Movement.PlayerHingeMovementController playerHingeController;
+
         [SerializeField] private Transform pfGlassWallBroken;
 
-        private Vector3 forceApplied = new Vector3(0, 50, 0);
+        public GameObject explosionGO;
 
+        //private Vector3 forceApplied = new Vector3(0, 50, 0);
 
+        public void BreakGlassWall() {
+            Instantiate(pfGlassWallBroken, transform.position, transform.rotation);
+            //Destroy(gameObject);
+            gameObject.transform.position = new Vector3(0, 0, 0);
+            Debug.Log("Glass Destroyed");
+
+            //pfGlassWallBroken.gameObject.GetComponent<Rigidbody>().AddExplosionForce(0, forceApplied, 0);
+        }
+
+        public IEnumerator WaitForAirTime() {
+
+            Debug.Log("WaitingForAirTime");
+
+            yield return new WaitForSeconds(.2f);
+
+            DestroyForceCollider();
+
+        }
+
+        public void DestroyForceCollider() {
+
+            Destroy(explosionGO);
+
+            Debug.Log("DestroyedExplosionGO");
+
+        }
+
+        //public Transform playerTransform;
+
+        /*
         private void OnCollisionEnter(Collision collision) {
-            
+
+            //playerHingeController.bWipeOutLocked = true;
+
+            Instantiate(pfGlassWallBroken, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Debug.Log("Glass Destroyed");
+
+            pfGlassWallBroken.gameObject.GetComponent<Rigidbody>().AddExplosionForce(0, forceApplied, 0);
+
             if (collision.gameObject.tag == "JacksFinish") {
 
-                Instantiate(pfGlassWallBroken, transform.position, transform.rotation);
-                Destroy(gameObject);
-                Debug.Log("Glass Destroyed");
-
-                pfGlassWallBroken.gameObject.GetComponent<Rigidbody>().AddExplosionForce(0, forceApplied, 0);
+                
 
             }
 
         }
+        */
 
     }
 }
