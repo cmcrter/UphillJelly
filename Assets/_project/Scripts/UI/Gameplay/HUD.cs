7c6bac107755c9089b50ee156c0938ac45ee8ff5
Matthew Mason <mattg.mason@outlook.com>
2022-04-05 17:50:02 +0100

    Cleaned up hud and remove unused anim events

--- Assets/_project/Scripts/UI/Gameplay/HUD.cs
+++ Assets/_project/Scripts/UI/Gameplay/HUD.cs
@@ -18,60 +18,81 @@ using System.Collections;
 
 public class HUD : MonoBehaviour
 {
-    #region Variables
-
-    //The General texts visible on the HUD
+    #region Private Serialized Fields
     [SerializeField]
-    private TextMeshProUGUI TrickText;
-    [SerializeField]
-    private TextMeshProUGUI ScoreText;
-
-    //For wiping out or finishing a combo, showing an image
-    [SerializeField]
-    private Image SoundImage;
-
-    [SerializeField]
-    private TrickBuffer trickBuffer;
-
-    [SerializeField]
-    private Color comboSuccessColor;
-
-    [SerializeField]
+    [Tooltip("The colour the trickComboText will turn when a combo is failed")]
     private Color comboFailedColor;
-
     [SerializeField]
+    [Tooltip("The colour the trickComboText will turn when a combo is completed")]
+    private Color comboSuccessColor;
+    [SerializeField]
+    [Tooltip("The colour the trickComboText will turn when a combo is failed")]
     private Color trickTextBaseColor;
 
     [SerializeField]
+    [Tooltip("How long it takes for the trick text to fade out once it is finished (Success or fail)")]
     private float trickTextFadeOutDuration;
 
+    //For wiping out or finishing a combo, showing an image
     [SerializeField]
-    private PlayerHingeMovementController playerControllingHud;
+    [Tooltip("For wiping out or finishing a combo, showing an image")]
+    private Image SoundImage;
 
-    private ScoreableAction actionInProgress;
+    [SerializeField]
+    [Tooltip("The player that the HUD is getting information from")]
+    private PlayerHingeMovementController controllingPlayer;
 
-    private ScoreableAction lastScoreableAction;
+    [SerializeField]
+    [Tooltip("The text displaying the overall score")]
+    private TextMeshProUGUI ScoreText;
+    //The General texts visible on the HUD
+    [SerializeField] 
+    [Tooltip("The text displaying the current trick combo")]
+    private TextMeshProUGUI trickComboText;
 
-    private int currentTrickCount;
 
-
-    private Timer trickComboTextClearTimer;
-
-    private Coroutine trickTextFadeOut;
-
-    private string comboString;
-
-    private float storedScore;
+    [SerializeField] 
+    [Tooltip("The trick buffer to get the current combo from")]
+    private TrickBuffer trickBuffer;
     #endregion
 
+    #region Private Variables
+    /// <summary>
+    /// The coroutine controlling the fading of the text
+    /// </summary>
+    private Coroutine trickTextFadeOut;
 
+    /// <summary>
+    /// The current total score for the player (this is place holder and should be gotten from its own score system)
+    /// </summary>
+    private float storedScore;
+
+    /// <summary>
+    /// The amount of the times the last trick has been the last completed trick in sequence
+    /// </summary>
+    private int currentTrickCount;
+
+    /// <summary>
+    /// The action that is currently in progress in the trick buffer
+    /// </summary>
+    private ScoreableAction actionInProgress;
+    /// <summary>
+    /// The last action completed received from the trick buffer
+    /// </summary>
+    private ScoreableAction lastScoreableAction;
+
+    /// <summary>
+    /// The text made up of all the combo pieces the player has done in the current combo
+    /// </summary>
+    private string comboString;
+
+    /// <summary>
+    /// The timer counting down the time till the ended combo text should have faded/
+    /// </summary>
+    private Timer trickComboTextFadeTimer;
+    #endregion
 
     #region Unity Methods
-    void Start()
-    {
-
-    }
-
     private void Awake()
     {
         // TODO: This will only work in single player, this should be one of the first things to fixed if local multiplayer is implemented
@@ -79,73 +100,104 @@ public class HUD : MonoBehaviour
         {
             trickBuffer = FindObjectOfType<TrickBuffer>();
         }
-        if (playerControllingHud == null)
+        if (controllingPlayer == null)
         {
-            playerControllingHud = FindObjectOfType<PlayerHingeMovementController>();
+            controllingPlayer = FindObjectOfType<PlayerHingeMovementController>();
         }
-    }
-
-    void Update()
-    {
-        ScoreText.text = Mathf.Round(storedScore).ToString();
-    }
-
-    private void OnEnable()
-    {
-        trickBuffer.ActionStarted += TrickBuffer_ActionStarted;
-        trickBuffer.ActionCompleted += TrickBuffer_ActionCompleted;
-
-        trickBuffer.ComboCompleted += TrickBuffer_ComboCompleted;
-        trickBuffer.ComboBroken += TrickBuffer_ComboBroken;
-
-        MoneyCollectable.MoneyPickedUp += MoneyCollectable_MoneyPickedUp;
-
-        playerControllingHud.onRespawn += PlayerControllingHud_onRespawn;
-    }
-
-    private void PlayerControllingHud_onRespawn()
-    {
-        HUDReset();
     }
 
     private void OnDisable()
     {
-        trickBuffer.ActionStarted -= TrickBuffer_ActionStarted;
-        trickBuffer.ActionCompleted -= TrickBuffer_ActionCompleted;
+        // Unbind from events
+        if (trickBuffer != null)
+        {
+            trickBuffer.ActionStarted -= TrickBuffer_ActionStarted;
+            trickBuffer.ActionCompleted -= TrickBuffer_ActionCompleted;
 
-        trickBuffer.ComboCompleted -= TrickBuffer_ComboCompleted;
-        trickBuffer.ComboBroken -= TrickBuffer_ComboBroken;
-
+            trickBuffer.ComboCompleted -= TrickBuffer_ComboCompleted;
+            trickBuffer.ComboBroken -= TrickBuffer_ComboBroken;
+        }
+        if (controllingPlayer != null)
+        {
+            controllingPlayer.onRespawn -= PlayerControllingHud_onRespawn;
+        }
         MoneyCollectable.MoneyPickedUp -= MoneyCollectable_MoneyPickedUp;
-
-        playerControllingHud.onRespawn -= PlayerControllingHud_onRespawn;
     }
 
+    private void OnEnable()
+    {
+        // Binding to events
+        if (trickBuffer != null)
+        {
+            trickBuffer.ActionStarted += TrickBuffer_ActionStarted;
+            trickBuffer.ActionCompleted += TrickBuffer_ActionCompleted;
+
+            trickBuffer.ComboCompleted += TrickBuffer_ComboCompleted;
+            trickBuffer.ComboBroken += TrickBuffer_ComboBroken;
+        }
+        if (controllingPlayer != null)
+        {
+            controllingPlayer.onRespawn += PlayerControllingHud_onRespawn;
+        }
+        MoneyCollectable.MoneyPickedUp += MoneyCollectable_MoneyPickedUp;
+    }
+
+    void Update()
+    {
+        // This will be changed when score has its own system
+        ScoreText.text = Mathf.Round(storedScore).ToString();
+    }
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Private Methods
+    private IEnumerator FadeOutTrickText(Color colorWhileFading, float fadeDuration)
+    {
+        trickComboTextFadeTimer = new Timer(fadeDuration);
+        while (trickComboTextFadeTimer.isActive)
+        {
+            trickComboTextFadeTimer.Tick(Time.deltaTime);
+            trickComboText.color = new Color(colorWhileFading.r, colorWhileFading.g, colorWhileFading.b, Mathf.Clamp01(trickComboTextFadeTimer.current_time / fadeDuration));
+            yield return null;
+        }
+        trickComboText.text = "";
+    }
+
+    /// <summary>
+    /// Called when the player has reset to reset elements of the HUD
+    /// </summary>
+    private void HUDReset()
+    {
+        // TODO: this should clear everything on character being reset
+
+        // Knock 20% off the score (Temporary, this script should not be controlling score)
+        storedScore *= 0.8f;
+    }
+    /// <summary>
+    /// Called when money is picked up to increase score (Temporary, this script should not be controlling score)
+    /// </summary>
+    /// <param name="playerPickingUpMoney">The player that picked up the money that triggered this event</param>
     private void MoneyCollectable_MoneyPickedUp(L7Games.Movement.PlayerController playerPickingUpMoney)
     {
-        if (playerPickingUpMoney == playerControllingHud)
+        if (playerPickingUpMoney == controllingPlayer)
         {
             storedScore += 10f;
         }
     }
-
-    private void TrickBuffer_ComboBroken()
+    /// <summary>
+    /// Called when the player that this HUD is displaying information for respawns
+    /// </summary>
+    private void PlayerControllingHud_onRespawn()
     {
-        comboString = "";
-        trickTextFadeOut = StartCoroutine(FadeOutTrickText(Color.red, trickTextFadeOutDuration));
-        lastScoreableAction = null;
-        actionInProgress = null;
+        HUDReset();
     }
-
-    private void TrickBuffer_ComboCompleted()
-    {
-        comboString = "";
-        trickTextFadeOut = StartCoroutine(FadeOutTrickText(Color.green, trickTextFadeOutDuration));
-        lastScoreableAction = null;
-        actionInProgress = null;
-        storedScore += trickBuffer.GetScoreFromCurrentCombo();
-    }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="completedAction"></param>
     private void TrickBuffer_ActionCompleted(ScoreableAction completedAction)
     {
         // Add action to the current text
@@ -153,7 +205,7 @@ public class HUD : MonoBehaviour
         if (actionInProgress == null && lastScoreableAction == null)
         {
             StopCoroutine(trickTextFadeOut);
-            TrickText.color = trickTextBaseColor;
+            trickComboText.color = trickTextBaseColor;
             comboString = completedAction.trickName;
             currentTrickCount = 1;
             lastScoreableAction = completedAction;
@@ -183,47 +235,27 @@ public class HUD : MonoBehaviour
         // Figure Out the score Text
         string comboScoreText = trickBuffer.GetScoreFromCurrentCombo().ToString() + " X " + trickBuffer.ComboMultiplier;
 
-        TrickText.text = comboScoreText + '\n' + comboString;
+        trickComboText.text = comboScoreText + '\n' + comboString;
     }
-
     private void TrickBuffer_ActionStarted(ScoreableAction obj)
     {
         //throw new System.NotImplementedException();
     }
-    #endregion
-
-    #region Public Methods
-
-    public void AddTrickToText(string trickName)
+    private void TrickBuffer_ComboBroken()
     {
-        
+        comboString = "";
+        trickTextFadeOut = StartCoroutine(FadeOutTrickText(comboFailedColor, trickTextFadeOutDuration));
+        lastScoreableAction = null;
+        actionInProgress = null;
+    }
+    private void TrickBuffer_ComboCompleted()
+    {
+        comboString = "";
+        trickTextFadeOut = StartCoroutine(FadeOutTrickText(comboSuccessColor, trickTextFadeOutDuration));
+        lastScoreableAction = null;
+        actionInProgress = null;
+        storedScore += trickBuffer.GetScoreFromCurrentCombo();
     }
 
-    public void UpdateScoreText(int score)
-    {
-        ScoreText.text = score.ToString();
-    }
-
-    public void HUDReset()
-    {
-        // TODO: this should clear everything on character being reset
-
-        // Knock 20% off the score
-        storedScore *= 0.8f;
-    }
-    #endregion
-
-    #region Private Methods
-    private IEnumerator FadeOutTrickText(Color colorWhileFading, float fadeDuration)
-    {
-        trickComboTextClearTimer = new Timer(fadeDuration);
-        while (trickComboTextClearTimer.isActive)
-        {
-            trickComboTextClearTimer.Tick(Time.deltaTime);
-            TrickText.color = new Color(colorWhileFading.r, colorWhileFading.g, colorWhileFading.b, Mathf.Clamp01(trickComboTextClearTimer.current_time / fadeDuration));
-            yield return null;
-        }
-        TrickText.text = "";
-    }
     #endregion
 }
