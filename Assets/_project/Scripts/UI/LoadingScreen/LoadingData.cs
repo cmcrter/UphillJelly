a8f10cc0f1b00e4b060e23ebe3d950a14770b7e8
Charles Carter <47723974+cmcrter@users.noreply.github.com>
2022-04-13 17:31:48 +0100

    Simple way to test tooltips

--- Assets/_project/Scripts/UI/LoadingScreen/LoadingData.cs
+++ Assets/_project/Scripts/UI/LoadingScreen/LoadingData.cs
@@ -3,19 +3,24 @@
 // Author: Charles Carter
 // Date Created: 04/02/2022
 // Last Edited By: Charles Carter
-// Date Last Edited: 04/02/2022
+// Date Last Edited: 13/04/2022
 // Brief: A small script to take over set data from the main menu to load in the loading screen
 //////////////////////////////////////////////////////////// 
 
-using UnityEngine;
+using System;
 
 namespace L7.Loading
 {
+    [Serializable]
     public class LoadingData
     {
         /// <summary>
         /// The static variable to carry which scene to load in the loading screen
         /// </summary>
         public static string sceneToLoad;
+        /// <summary>
+        /// The static variable for the loading screen to know if something needs to be pressed to go to next scene
+        /// </summary>
+        public static bool waitForNextScene;
     }
 }
